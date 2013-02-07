9a1f140adb58f3ac27d032b9e34a9541827afa7a
Alex Salazar <alex.salazar@ec.pe>
2013-02-06 20:02:58 -0500

    cometarios

--- system/cms/logs/log-2013-02-07.php
+++ system/cms/logs/log-2013-02-07.php
@@ -32,3 +32,15 @@ ERROR - 2013-02-07 00:52:14 --> Severity: Notice  --> Undefined index: en C:\xam
 ERROR - 2013-02-07 00:52:14 --> Severity: Notice  --> Undefined index: en C:\xampp\htdocs\adminmicanal\system\cms\modules\modules\models\module_m.php 144
 ERROR - 2013-02-07 00:52:14 --> Severity: Notice  --> Undefined index: en C:\xampp\htdocs\adminmicanal\system\cms\modules\modules\models\module_m.php 70
 ERROR - 2013-02-07 00:52:14 --> Severity: Notice  --> Undefined index: en C:\xampp\htdocs\adminmicanal\system\cms\modules\modules\models\module_m.php 71
+ERROR - 2013-02-07 01:07:07 --> Severity: Warning  --> mysql_connect(): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\adminmicanal\system\codeigniter\database\drivers\mysql\mysql_driver.php 84
+ERROR - 2013-02-07 01:07:07 --> Unable to connect to the database
+ERROR - 2013-02-07 01:07:42 --> Page Missing: canales/videos/carga_unitaria
+ERROR - 2013-02-07 01:08:06 --> Severity: Notice  --> unserialize(): Error at offset 77 of 39766 bytes C:\xampp\htdocs\adminmicanal\system\cms\libraries\Simplepie.php 230
+ERROR - 2013-02-07 01:13:05 --> Unable to select database: pyro_admin
+ERROR - 2013-02-07 01:13:15 --> Unable to select database: pyro_admin
+ERROR - 2013-02-07 01:13:18 --> Unable to select database: pyro_admin
+ERROR - 2013-02-07 01:13:58 --> Severity: Notice  --> Undefined variable: opc_canales C:\xampp\htdocs\adminmicanal\addons\shared_addons\modules\videos\details.php 34
+ERROR - 2013-02-07 01:13:59 --> Severity: Notice  --> Undefined variable: opc_canales C:\xampp\htdocs\adminmicanal\addons\shared_addons\modules\videos\details.php 34
+ERROR - 2013-02-07 01:14:03 --> Severity: Notice  --> Undefined variable: opc_canales C:\xampp\htdocs\adminmicanal\addons\shared_addons\modules\videos\details.php 34
+ERROR - 2013-02-07 01:59:55 --> Page Missing: canales/videos/carga_unitaria
+ERROR - 2013-02-07 01:59:58 --> Page Missing: canales/videos/carga_unitaria
