af9912faaf6f7980ee172f69e6b2a7778279aa5f
Alex Salazar <alex.salazar@ec.pe>
2013-03-11 12:35:49 -0500

    changes

--- system/cms/logs/log-2013-02-08.php
+++ system/cms/logs/log-2013-02-08.php
@@ -3,3 +3,10 @@
 ERROR - 2013-02-08 00:18:04 --> Query error: Unknown column 'v.horario_transmision' in 'field list'
 ERROR - 2013-02-08 00:49:22 --> Severity: Notice  --> Undefined property: stdClass::$horario_transmision /var/www/admin3.micanal.dev/addons/shared_addons/modules/canales/views/admin/videos.php 63
 ERROR - 2013-02-08 00:54:52 --> Severity: Notice  --> Undefined index: pesonajes /var/www/admin3.micanal.dev/addons/shared_addons/modules/tags/models/tags_m.php 81
+ERROR - 2013-02-08 20:50:16 --> Severity: Notice  --> unserialize(): Error at offset 77 of 39675 bytes C:\xampp\htdocs\adminmicanal\system\cms\libraries\Simplepie.php 230
+ERROR - 2013-02-08 20:50:21 --> Query error: Unknown column 'v.horario_transmision_inicio' in 'field list'
+ERROR - 2013-02-08 20:51:41 --> Query error: Unknown column 'v.horario_transmision_inicio' in 'field list'
+ERROR - 2013-02-08 20:52:33 --> Query error: Unknown column 'v.horario_transmision_inicio' in 'field list'
+ERROR - 2013-02-08 20:52:38 --> Query error: Unknown column 'v.horario_transmision_inicio' in 'field list'
+ERROR - 2013-02-08 20:54:48 --> Query error: Unknown column 'v.horario_transmision_inicio' in 'field list'
+ERROR - 2013-02-08 21:01:20 --> Query error: Unknown column 'v.horario_transmision_inicio' in 'field list'
---
+++ system/cms/logs/log-2013-02-11.php
@@ -0,0 +1,3 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-02-11 16:07:35 --> Query error: Unknown column 'v.horario_transmision_inicio' in 'field list'
