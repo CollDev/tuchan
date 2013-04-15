48adc846adaf6ba8b56b1e53ddb42260491ca533
Alex Salazar <alex.salazar@ec.pe>
2013-04-15 18:46:13 -0500

    changes

--- system/cms/themes/pyrocms/js/scripts.js
+++ system/cms/themes/pyrocms/js/scripts.js
@@ -515,3 +515,12 @@ jQuery(function($) {
 		});
 	});
 });
+
+
+
+var bkg=$("section.title").find("h4").attr("background");
+var nam=$("section.title").find("h4").html();
+console.log(bkg);
+$("channel_item").css("background",bkg);
+$("channel_item").find("a").before("<span>"+nam+"</span>");
+
