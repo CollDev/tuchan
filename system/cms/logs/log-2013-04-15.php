0ee3a0ff8f7afa8ab04f266137c5a2a8e7edcf96
martin <martin.programmaer@gmail.com>
2013-04-15 15:23:25 -0500

    CMS update

--- system/cms/logs/log-2013-04-15.php
+++ system/cms/logs/log-2013-04-15.php
@@ -4,3 +4,5 @@ ERROR - 2013-04-15 17:28:06 --> Severity: Notice  --> Undefined offset: 0 /home/
 ERROR - 2013-04-15 17:28:06 --> Severity: Notice  --> Trying to get property of non-object /home/user/Escritorio/adminmicanal/addons/shared_addons/modules/canales/views/admin/videos.php 4
 ERROR - 2013-04-15 17:28:09 --> Severity: Notice  --> Undefined offset: 0 /home/user/Escritorio/adminmicanal/addons/shared_addons/modules/canales/views/admin/videos.php 4
 ERROR - 2013-04-15 17:28:09 --> Severity: Notice  --> Trying to get property of non-object /home/user/Escritorio/adminmicanal/addons/shared_addons/modules/canales/views/admin/videos.php 4
+ERROR - 2013-04-15 20:16:07 --> Severity: Notice  --> Undefined property: CI::$imagenes_m /var/www/local.adminmicanal.com/system/cms/libraries/MX/Controller.php 57
+ERROR - 2013-04-15 20:21:45 --> Severity: Notice  --> unserialize(): Error at offset 40858 of 47187 bytes /var/www/local.adminmicanal.com/system/cms/libraries/Simplepie.php 230
