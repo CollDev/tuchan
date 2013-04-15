c5cd720929dcff80fbf1c0f39004ac22343e256c
Alex Salazar <alex.salazar@ec.pe>
2013-04-15 18:19:22 -0500

    changes

---
+++ system/cms/themes/pyrocms/js/fix_channels.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+var bkg=$("section.title").find("h4").attr("background");
+var nam=$("section.title").find("h4").html();
+console.log(bkg);
+$("channel_item").css("background",bkg);
+$("channel_item").find("a").before("<span>"+nam+"</span>");
+
