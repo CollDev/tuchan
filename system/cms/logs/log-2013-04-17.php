30a25512a2479915727d73d7b45df51a0a6ba368
user <user@intel-desktop.(none)>
2013-04-17 11:15:42 -0500

    cambios

--- system/cms/logs/log-2013-04-17.php
+++ system/cms/logs/log-2013-04-17.php
@@ -4,3 +4,25 @@ ERROR - 2013-04-17 00:04:43 --> Severity: Notice  --> Undefined variable: result
 ERROR - 2013-04-17 00:04:47 --> Severity: Notice  --> Undefined property: CI::$conexionmysql /home/user/Escritorio/adminmicanal/system/cms/libraries/MX/Controller.php 57
 ERROR - 2013-04-17 00:07:20 --> Severity: Notice  --> Undefined variable: result /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 19
 ERROR - 2013-04-17 00:07:25 --> Severity: Notice  --> Undefined variable: result /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 19
+ERROR - 2013-04-17 14:55:13 --> Severity: Warning  --> Missing argument 1 for Proceso_m::queryMysqlMiCanal(), called in /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php on line 26 and defined /home/user/Escritorio/adminmicanal/system/cms/models/proceso_m.php 19
+ERROR - 2013-04-17 14:55:13 --> Severity: Notice  --> Undefined variable: option /home/user/Escritorio/adminmicanal/system/cms/models/proceso_m.php 22
+ERROR - 2013-04-17 14:55:13 --> Severity: Notice  --> Undefined variable: option /home/user/Escritorio/adminmicanal/system/cms/models/proceso_m.php 25
+ERROR - 2013-04-17 14:55:13 --> Severity: Notice  --> Undefined variable: option /home/user/Escritorio/adminmicanal/system/cms/models/proceso_m.php 28
+ERROR - 2013-04-17 14:55:13 --> Severity: Notice  --> Undefined variable: option /home/user/Escritorio/adminmicanal/system/cms/models/proceso_m.php 31
+ERROR - 2013-04-17 14:55:13 --> Severity: Notice  --> Undefined variable: option /home/user/Escritorio/adminmicanal/system/cms/models/proceso_m.php 34
+ERROR - 2013-04-17 14:55:13 --> Severity: Notice  --> Undefined variable: query /home/user/Escritorio/adminmicanal/system/cms/models/proceso_m.php 39
+ERROR - 2013-04-17 14:55:13 --> Invalid query: 
+ERROR - 2013-04-17 15:30:51 --> Severity: Warning  --> Missing argument 3 for Proceso::corteVideo() /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 16
+ERROR - 2013-04-17 15:30:51 --> Severity: Warning  --> Missing argument 4 for Proceso::corteVideo() /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 16
+ERROR - 2013-04-17 15:33:20 --> Severity: Notice  --> Undefined property: CI::$proceso_m /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 22
+ERROR - 2013-04-17 15:33:21 --> Severity: Notice  --> Undefined property: CI::$proceso_m /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 22
+ERROR - 2013-04-17 15:48:33 --> Severity: Notice  --> Use of undefined constant video_path - assumed 'video_path' /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 18
+ERROR - 2013-04-17 15:48:52 --> Severity: Notice  --> Undefined variable: config /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 18
+ERROR - 2013-04-17 15:49:08 --> Severity: Notice  --> Undefined variable: config /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 18
+ERROR - 2013-04-17 15:49:09 --> Severity: Notice  --> Undefined variable: config /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 18
+ERROR - 2013-04-17 15:49:09 --> Severity: Notice  --> Undefined variable: config /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 18
+ERROR - 2013-04-17 15:49:34 --> Severity: Notice  --> Undefined property: MX_Config::$video_path /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 18
+ERROR - 2013-04-17 15:49:34 --> Severity: Notice  --> Undefined property: MX_Config::$video_path /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 18
+ERROR - 2013-04-17 15:49:35 --> Severity: Notice  --> Undefined property: MX_Config::$video_path /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 18
+ERROR - 2013-04-17 15:51:03 --> Severity: Notice  --> Undefined variable: id_video /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 20
+ERROR - 2013-04-17 16:05:20 --> Severity: Notice  --> Undefined variable: result /home/user/Escritorio/adminmicanal/system/cms/controllers/proceso.php 18
