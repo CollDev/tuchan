4be482f37cd6a65b14b6c706acd34c0119bfba2e
martin <martin.programmaer@gmail.com>
2013-04-20 20:50:33 -0500

    actualizacion

--- system/cms/config/database.php
+++ system/cms/config/database.php
@@ -32,7 +32,6 @@
 */
 
 // Development
-/*
 $db[PYRO_DEVELOPMENT] = array(
 	'hostname'		=> 	'192.168.1.35', //'10.203.31.139'
 	'username'		=> 	'root',//'micanalcmsdev',
@@ -51,9 +50,10 @@ $db[PYRO_DEVELOPMENT] = array(
 	// 'Tough love': Forces strict mode to test your app for best compatibility
 	'stricton' 		=> TRUE,
 );
-*/
+
 
 // El Comercio
+/*
 $db[PYRO_DEVELOPMENT] = array(
 	'hostname'		=> 	'10.203.31.139',
 	'username'		=> 	'micanalcmsdev',
@@ -72,7 +72,7 @@ $db[PYRO_DEVELOPMENT] = array(
 	// 'Tough love': Forces strict mode to test your app for best compatibility
 	'stricton' 		=> TRUE,
 );
-
+*/
 
 // Staging
 /*
