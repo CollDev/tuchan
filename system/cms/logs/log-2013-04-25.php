97bda1b24e972759e6a4a1c134aeb7ba6335cee6
martin <martin.programmaer@gmail.com>
2013-04-24 19:40:47 -0500

    fixed

--- system/cms/logs/log-2013-04-25.php
+++ system/cms/logs/log-2013-04-25.php
@@ -6,3 +6,9 @@ ERROR - 2013-04-25 00:39:31 --> Severity: Warning  --> mysqli_connect(): (HY000/
 ERROR - 2013-04-25 00:39:31 --> Unable to connect to the database
 ERROR - 2013-04-25 00:39:36 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (101) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
 ERROR - 2013-04-25 00:39:36 --> Unable to connect to the database
+ERROR - 2013-04-25 00:39:56 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (101) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-04-25 00:39:56 --> Unable to connect to the database
+ERROR - 2013-04-25 00:40:16 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (101) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-04-25 00:40:16 --> Unable to connect to the database
+ERROR - 2013-04-25 00:40:36 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (101) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-04-25 00:40:36 --> Unable to connect to the database
