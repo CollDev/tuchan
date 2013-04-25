1ada657883f53799797e6971a566abbee8926b43
martin <martin.programmaer@gmail.com>
2013-04-24 19:39:59 -0500

    fixed

--- system/cms/logs/log-2013-04-21.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-04-21 00:11:08 --> Severity: Warning  --> error_log() expects parameter 1 to be string, object given /home/user/Escritorio/adminmicanal/addons/shared_addons/libraries/procesos_lib.php 223
-ERROR - 2013-04-21 00:11:43 --> Severity: Warning  --> error_log() expects parameter 1 to be string, object given /home/user/Escritorio/adminmicanal/addons/shared_addons/libraries/procesos_lib.php 223
-ERROR - 2013-04-21 00:12:01 --> Severity: Warning  --> error_log() expects parameter 1 to be string, object given /home/user/Escritorio/adminmicanal/addons/shared_addons/libraries/procesos_lib.php 223
-ERROR - 2013-04-21 00:14:52 --> Severity: Warning  --> error_log() expects parameter 1 to be string, object given /home/user/Escritorio/adminmicanal/addons/shared_addons/libraries/procesos_lib.php 225
---
+++ system/cms/logs/log-2013-04-25.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-04-25 00:39:27 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (101) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-04-25 00:39:27 --> Unable to connect to the database
+ERROR - 2013-04-25 00:39:31 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (101) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-04-25 00:39:31 --> Unable to connect to the database
+ERROR - 2013-04-25 00:39:36 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (101) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-04-25 00:39:36 --> Unable to connect to the database
