45883573ff2caf164bd2b2a4f339eea50e32383e
martin <martin.programmaer@gmail.com>
2013-04-30 16:24:17 -0500

    fixed canales

---
+++ system/cms/logs/log-2013-04-30.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-04-30 21:18:07 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-04-30 21:18:07 --> Unable to connect to the database
+ERROR - 2013-04-30 21:18:11 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-04-30 21:18:11 --> Unable to connect to the database
+ERROR - 2013-04-30 21:19:14 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-04-30 21:19:14 --> Unable to connect to the database
+ERROR - 2013-04-30 21:20:20 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-04-30 21:20:20 --> Unable to connect to the database
