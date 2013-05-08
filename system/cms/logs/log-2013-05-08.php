323cacedc7b2bec8d780cc013ee9d0d3ac19859c
user <user@intel-desktop.(none)>
2013-05-08 16:45:53 -0500

    fixed

--- system/cms/logs/log-2013-05-08.php
+++
@@ -1,2 +0,0 @@
-ERROR - 2013-05-08 21:29:42 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
-ERROR - 2013-05-08 21:29:42 --> Unable to connect to the database
--- system/cms/models/videos_mp.php
+++ system/cms/models/videos_mp.php
@@ -22,7 +22,7 @@ class Videos_mp extends CI_Model {
     }
 
     public function getVideosActivos() {
-        $query = "select id,id_mongo from " . $this->_table . " WHERE codigo  IS NOT NULL and estado_liquid =0 and estado=1 ";
+        $query = "select id,id_mongo from " . $this->_table . " WHERE codigo  IS NOT NULL and estado_liquid =0 ";
         return $this->db->query($query)->result();
     }
 
