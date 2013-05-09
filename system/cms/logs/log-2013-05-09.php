5fab9a9f9a34f34405e35690b8f0835cc86cf254
martin <martin.programmaer@gmail.com>
2013-05-09 18:17:47 -0500

    fixed

---
+++ system/cms/logs/log-2013-05-09.php
@@ -0,0 +1,4 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-05-09 00:05:02 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /var/www/local.adminmicanal.com/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-05-09 00:05:02 --> Unable to connect to the database
