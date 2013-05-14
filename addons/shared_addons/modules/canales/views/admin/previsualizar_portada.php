c288fb5f6c8005034a25e2d55f50c6aeaa507603
aldodiaz <hdegues@gmail.com>
2013-05-14 17:32:17 -0500

    vista previa portada

--- addons/shared_addons/modules/canales/views/admin/previsualizar_portada.php
+++ addons/shared_addons/modules/canales/views/admin/previsualizar_portada.php
@@ -1,8 +1,5 @@
 <link href="<?php echo base_url('/addons/shared_addons/modules/canales/css/custom.css') ?>" rel="stylesheet" />
 <link href="<?php echo base_url('/addons/shared_addons/modules/canales/css/mediaquerie.css') ?>" rel="stylesheet" />
-<pre>
-<?php print_r($objDestacado); ?>
-</pre>
 <div class="container-main">
     <div class="wrapper-main">
         <div class="mc_column mc_columnA mc_mbottom">
