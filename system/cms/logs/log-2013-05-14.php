e883bf28d8b4f9bec0657c9cf52f9f086367157c
user <user@intel-desktop.(none)>
2013-05-14 11:43:18 -0500

    fixed

--- system/cms/logs/log-2013-05-14.php
+++ system/cms/logs/log-2013-05-14.php
@@ -20,3 +20,9 @@ ERROR - 2013-05-14 04:13:05 --> Severity: Notice  --> Undefined variable: arrima
 ERROR - 2013-05-14 04:13:05 --> Severity: Notice  --> Undefined variable: arrimagen /home/user/Escritorio/adminmicanal/addons/shared_addons/libraries/procesos_lib.php 1576
 ERROR - 2013-05-14 04:50:55 --> Severity: Warning  --> mysqli_num_rows() expects parameter 1 to be mysqli_result, boolean given /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_result.php 46
 ERROR - 2013-05-14 04:50:55 --> Severity: Warning  --> mysqli_num_rows() expects parameter 1 to be mysqli_result, boolean given /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_result.php 46
+ERROR - 2013-05-14 16:39:24 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-05-14 16:39:24 --> Unable to connect to the database
+ERROR - 2013-05-14 16:40:28 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-05-14 16:40:28 --> Unable to connect to the database
+ERROR - 2013-05-14 16:40:28 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-05-14 16:40:28 --> Unable to connect to the database
