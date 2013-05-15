8780e9648c310bf27a1be2d61c2461201be0b36e
user <user@intel-desktop.(none)>
2013-05-15 10:26:14 -0500

    fixed

--- system/cms/logs/log-2013-05-15.php
+++ system/cms/logs/log-2013-05-15.php
@@ -8,3 +8,32 @@ ERROR - 2013-05-15 00:02:01 --> Severity: Warning  --> mysqli_connect(): (HY000/
 ERROR - 2013-05-15 00:02:01 --> Unable to connect to the database
 ERROR - 2013-05-15 00:05:21 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
 ERROR - 2013-05-15 00:05:21 --> Unable to connect to the database
+ERROR - 2013-05-15 15:23:54 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:23:54 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:23:54 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:23:54 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:23:54 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:23:54 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:23:54 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:23:54 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:23:54 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:24:14 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:24:14 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:24:14 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:24:14 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:24:14 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:24:14 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:24:14 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:24:14 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:24:14 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:24:34 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:24:34 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:24:34 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:24:34 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:24:34 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:24:34 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:24:34 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:24:34 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 142
+ERROR - 2013-05-15 15:24:34 --> Severity: Notice  --> Undefined index: es /home/user/Escritorio/adminmicanal/system/cms/modules/modules/models/module_m.php 143
+ERROR - 2013-05-15 15:25:57 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-05-15 15:25:57 --> Unable to connect to the database
