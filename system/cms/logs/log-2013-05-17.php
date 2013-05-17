cbeb75cfe3d043923eded577d615d3e2a7ff2d51
user <user@intel-desktop.(none)>
2013-05-16 20:36:21 -0500

    fixed

--- system/cms/logs/log-2013-05-17.php
+++ system/cms/logs/log-2013-05-17.php
@@ -14,3 +14,9 @@ ERROR - 2013-05-17 00:30:53 --> Severity: Warning  --> mysqli_connect(): (HY000/
 ERROR - 2013-05-17 00:30:53 --> Unable to connect to the database
 ERROR - 2013-05-17 00:31:13 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
 ERROR - 2013-05-17 00:31:13 --> Unable to connect to the database
+ERROR - 2013-05-17 01:35:11 --> Severity: Notice  --> Undefined variable: db /home/user/Escritorio/adminmicanal/system/cms/config/database.php 116
+ERROR - 2013-05-17 01:35:11 --> Severity: Warning  --> array_key_exists() expects parameter 2 to be array, null given /home/user/Escritorio/adminmicanal/system/cms/config/database.php 116
+ERROR - 2013-05-17 01:35:31 --> Severity: Notice  --> Undefined variable: db /home/user/Escritorio/adminmicanal/system/cms/config/database.php 116
+ERROR - 2013-05-17 01:35:31 --> Severity: Warning  --> array_key_exists() expects parameter 2 to be array, null given /home/user/Escritorio/adminmicanal/system/cms/config/database.php 116
+ERROR - 2013-05-17 01:35:51 --> Severity: Notice  --> Undefined variable: db /home/user/Escritorio/adminmicanal/system/cms/config/database.php 116
+ERROR - 2013-05-17 01:35:51 --> Severity: Warning  --> array_key_exists() expects parameter 2 to be array, null given /home/user/Escritorio/adminmicanal/system/cms/config/database.php 116
