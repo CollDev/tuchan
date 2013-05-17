6f013ca0b90e3a697c8e7a31fc6afcc17d7ae703
user <user@intel-desktop.(none)>
2013-05-17 17:42:12 -0500

    fixed

--- system/cms/config/config.php
+++ system/cms/config/config.php
@@ -397,16 +397,19 @@ $config['modules_locations'] = array(
             $config['protocolo:http'] = 'http:\/\/';
 
 //servidor elemento
+            
+/*
 $config['server:elemento'] = 'dev.e.micanal.e3.pe';
 $config['url:elemento'] = 'http://dev.e3.pe/index.php/api/v1';
 $config['apikey:elemento'] = '590ee43e919b1f4baa2125a424f03cd160ff8901';
 $config['mensaje:elemento'] = 'cms.micanal.pe';
+*/
 
 //servidor pre
-//$config['server:elemento'] = 'pre.e.micanal.e3.pe';
-//$config['url:elemento'] = 'http://pre.e3.pe/index.php/api/v1';
-//$config['apikey:elemento'] = '590ee43e919b1f4baa2125a424f03cd160ff8901';
-
+$config['server:elemento'] = 'pre.e.micanal.e3.pe';
+$config['url:elemento'] = 'http://pre.e3.pe/index.php/api/v1';
+$config['apikey:elemento'] = '590ee43e919b1f4baa2125a424f03cd160ff8901';
+$config['mensaje:elemento'] = 'cms.micanal.pe';
 
 //rutas de uploads
 $config['path:video'] = FCPATH.'uploads/videos/';
@@ -429,7 +432,12 @@ $config['migracion:paginas'] = '10';
 $config['migracion:margen_error_imagen'] = '20';
 
 
-$config['motor'] = 'http://dev.micanal.pe';
+/* DEV */ 
+// $config['motor'] = 'http://dev.micanal.pe';
+
+/* PRE */ 
+$config['motor'] = 'http://pre.micanal.pe';
+
 
 
 /* End of file config.php */
