07a55edde6921cf7b2bff0d0d9ba9cc92ccad955
user <user@intel-desktop.(none)>
2013-05-20 10:39:20 -0500

    fixed

--- system/cms/models/micanal_mp.php
+++ system/cms/models/micanal_mp.php
@@ -59,8 +59,12 @@ class MiCanal_mp extends CI_Model {
 //         return $resultado;
 
         $objresult = $this->db->query($query);
-        mysqli_next_result($this->db->conn_id);
-        return $objresult->result();
+        if(!empty($objresult)){
+            mysqli_next_result($this->db->conn_id);
+            return $objresult->result();
+        }else{
+            return "";
+        }                   
     }
 
     public function queryMysql($option, $id = "") {
