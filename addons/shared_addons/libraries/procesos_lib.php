da7d17e11b3002b5c0b4ffb7638454468fb405e9
user <user@intel-desktop.(none)>
2013-05-20 10:33:18 -0500

    fixed

--- addons/shared_addons/libraries/procesos_lib.php
+++ addons/shared_addons/libraries/procesos_lib.php
@@ -1329,7 +1329,7 @@ class Procesos_lib extends MX_Controller {
             foreach ($canal as $value) {
                  Log::erroLog("_generarCanalesXId : ". $id. "  estado:  ". $value->estado . " >> ". $value->estado_migracion );
 
-                if ( $value->estado == 2) {
+                if ( $value->estado == 1) {
 
                     $objmongo = array();
                     $objmongo['canal'] = strip_tags($value->nombre);
