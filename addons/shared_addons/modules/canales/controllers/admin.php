ec83263531026ccb9fe6d44c3cfa89df8fc8ce17
aldodiaz <hdegues@gmail.com>
2013-05-29 12:42:18 -0500

    corregir buscador para las secciones los+

--- addons/shared_addons/modules/canales/controllers/admin.php
+++ addons/shared_addons/modules/canales/controllers/admin.php
@@ -1891,11 +1891,10 @@ class Admin extends Admin_Controller {
             $primero = $objPrimero;
         }
         //lista de templates
-        
         //$templates = $this->templates_m->getTemplateDropDown();
         //$templates = $this->templates_m->getTemplateDropDown(array("id" => $objSeccion->templates_id));
         $objTemplate = $this->templates_m->get($objSeccion->templates_id);
-        $templates = array($objTemplate->id=>$objTemplate->nombre);
+        $templates = array($objTemplate->id => $objTemplate->nombre);
         //tipo de secciones
         $secciones = $this->tipo_secciones_m->getSeccionDropDown();
         $this->input->is_ajax_request() and $this->template->set_layout(FALSE);
@@ -3924,6 +3923,12 @@ class Admin extends Admin_Controller {
         }
     }
 
+    /**
+     * Método para listar items que correspondan al formulario de busqueda solo para destacados
+     * @author Johnny Huamani <johnny1402@gmail.com>
+     * @param int $current_page
+     * @param int $paginado
+     */
     public function buscar_para_destacado($current_page = 1, $paginado = 0) {
         if ($this->input->is_ajax_request()) {
             //listamos todos los maestros
@@ -4617,39 +4622,46 @@ class Admin extends Admin_Controller {
         }
     }
 
+    /**
+     * Método para listar items para las secciones los más recientes, los más buscados, los + etc
+     * @author Johnny Huamani <johnny1402@gmail.com>
+     * @param int $current_page
+     * @param int $paginado
+     */
     public function buscar_para_losmas($current_page = 1, $paginado = 0) {
         if ($this->input->is_ajax_request()) {
             //listamos todos los maestros
-            if (strlen(trim($this->input->post('txtBuscar'))) > 0) {
-                $lista_maestros = $this->grupo_maestro_m->like('nombre', $this->input->post('txtBuscar'))->get_many_by(array('canales_id' => $this->input->post('canal_id'), "tipo_grupo_maestro_id" => $this->config->item('videos:lista')));
-            } else {
-                $lista_maestros = $this->grupo_maestro_m->get_many_by(array('canales_id' => $this->input->post('canal_id'), "tipo_grupo_maestro_id" => $this->config->item('videos:lista')));
-            }
             $array_maestros = array();
-            if (count($lista_maestros) > 0) {
+            /* if (strlen(trim($this->input->post('txtBuscar'))) > 0) {
+              $lista_maestros = $this->grupo_maestro_m->like('nombre', $this->input->post('txtBuscar'))->get_many_by(array('canales_id' => $this->input->post('canal_id'), "tipo_grupo_maestro_id" => $this->config->item('videos:lista')));
+              } else {
+              $lista_maestros = $this->grupo_maestro_m->get_many_by(array('canales_id' => $this->input->post('canal_id'), "tipo_grupo_maestro_id" => $this->config->item('videos:lista')));
+              } */
 
-                foreach ($lista_maestros as $puntero => $objMaestro) {
-                    if (count($objMaestro) > 0) {
-                        $objMaestro->es_maestro = 1;
-                        $objTipoMaestro = $this->tipo_maestro_m->get($objMaestro->tipo_grupo_maestro_id);
-                        $objMaestro->tipo = $objTipoMaestro->nombre;
-                        array_push($array_maestros, $objMaestro);
-                    }
-                }
-                //obtenemos los videos para listarlos
-                if (strlen(trim($this->input->post('txtBuscar'))) > 0) {
-                    $lista_videos = $this->videos_m->like('titulo', $this->input->post('txtBuscar'))->get_many_by(array('canales_id' => $this->input->post('canal_id')));
-                } else {
-                    $lista_videos = $this->videos_m->get_many_by(array('canales_id' => $this->input->post('canal_id')));
-                }
-                if (count($lista_videos) > 0) {
-                    foreach ($lista_videos as $index => $objVideo) {
-                        $objVideo->es_maestro = 0;
-                        $objVideo->tipo = 'Video';
-                        array_push($array_maestros, $objVideo);
-                    }
+            //if (count($lista_maestros) > 0) {
+
+            /* foreach ($lista_maestros as $puntero => $objMaestro) {
+              if (count($objMaestro) > 0) {
+              $objMaestro->es_maestro = 1;
+              $objTipoMaestro = $this->tipo_maestro_m->get($objMaestro->tipo_grupo_maestro_id);
+              $objMaestro->tipo = $objTipoMaestro->nombre;
+              array_push($array_maestros, $objMaestro);
+              }
+              } */
+            //obtenemos los videos para listarlos
+            if (strlen(trim($this->input->post('txtBuscar'))) > 0) {
+                $lista_videos = $this->videos_m->like('titulo', $this->input->post('txtBuscar'))->get_many_by(array('canales_id' => $this->input->post('canal_id')));
+            } else {
+                $lista_videos = $this->videos_m->get_many_by(array('canales_id' => $this->input->post('canal_id')));
+            }
+            if (count($lista_videos) > 0) {
+                foreach ($lista_videos as $index => $objVideo) {
+                    $objVideo->es_maestro = 0;
+                    $objVideo->tipo = 'Video';
+                    array_push($array_maestros, $objVideo);
                 }
             }
+            //}
             $total = count($array_maestros);
             $cantidad_mostrar = 7;
             $current_page = $current_page - 1;
@@ -5013,6 +5025,12 @@ class Admin extends Admin_Controller {
         }
     }
 
+    /**
+     * Método para listar items de solo videos para este tipo de seccion
+     * @author Johnny Huamani <johnny1402@gmail.com>
+     * @param int $current_page
+     * @param int $paginado
+     */
     public function buscar_para_losmas_programa($current_page = 1, $paginado = 0) {
         if ($this->input->is_ajax_request()) {
             $objSeccion = $this->secciones_m->get($this->input->post('seccion_id'));
@@ -5021,7 +5039,7 @@ class Admin extends Admin_Controller {
             $array_maestros = array();
             $array_id_item = array();
             //listamos todas las listas de las colecciones del programa
-            $lista_programa = $this->obtenerListaPrograma($programa_id);
+            /*$lista_programa = $this->obtenerListaPrograma($programa_id);
             if (count($lista_programa) > 0) {
                 foreach ($lista_programa as $puntero => $objMaestro) {
                     if (strlen(trim($this->input->post('txtBuscar'))) > 0) {
@@ -5040,9 +5058,9 @@ class Admin extends Admin_Controller {
                         array_push($array_id_item, $objMaestro->id);
                     }
                 }
-            }
+            }*/
             //listamos las listas diractas al programa
-            $lista_directas_programa = $this->obtenerListaProgramaDirecta($programa_id);
+            /*$lista_directas_programa = $this->obtenerListaProgramaDirecta($programa_id);
             if (count($lista_directas_programa) > 0) {
                 foreach ($lista_directas_programa as $indice => $objMaestro2) {
                     if (strlen(trim($this->input->post('txtBuscar'))) > 0) {
@@ -5061,9 +5079,9 @@ class Admin extends Admin_Controller {
                         array_push($array_id_item, $objMaestro2->id);
                     }
                 }
-            }
+            }*/
             //limpiamos las listas repetidas
-            if (count($array_id_item) > 0) {
+            /*if (count($array_id_item) > 0) {
                 $array_id_item = array_unique($array_id_item);
                 $array_maestros = array();
                 foreach ($array_id_item as $in => $cod_maestro) {
@@ -5073,7 +5091,7 @@ class Admin extends Admin_Controller {
                     $objMaestro3->tipo = $objTipoMaestro->nombre;
                     array_push($array_maestros, $objMaestro3);
                 }
-            }
+            }*/
             //listamos los videos del programa
             $detalle_videos_programa = $this->obtenerVideosPrograma($programa_id);
             if (count($detalle_videos_programa) > 0) {
