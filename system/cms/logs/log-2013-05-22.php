497e0609ebcbd82bed7ee7f629646e262a038b3c
user <user@intel-desktop.(none)>
2013-05-28 19:40:12 -0500

    fixed

--- system/cms/logs/log-2013-05-22.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-05-22 23:03:45 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
-ERROR - 2013-05-22 23:03:45 --> Unable to connect to the database
-ERROR - 2013-05-22 23:04:48 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
-ERROR - 2013-05-22 23:04:48 --> Unable to connect to the database
---
+++ system/cms/logs/log-2013-05-29.php
@@ -0,0 +1,4 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-05-29 00:39:19 --> Severity: Warning  --> mysqli_connect(): (HY000/2003): Can't connect to MySQL server on '10.203.31.139' (110) /home/user/Escritorio/adminmicanal/system/codeigniter/database/drivers/mysqli/mysqli_driver.php 75
+ERROR - 2013-05-29 00:39:19 --> Unable to connect to the database
