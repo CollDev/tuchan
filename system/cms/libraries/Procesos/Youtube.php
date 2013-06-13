cbef6672cda67bb64a7098821938705ba74bfc9f
user <user@intel-desktop.(none)>
2013-06-12 19:06:54 -0500

    fixed

--- system/cms/libraries/Procesos/Youtube.php
+++ system/cms/libraries/Procesos/Youtube.php
@@ -1,45 +1,47 @@
 <?php
 
-
 class Youtube {
 
-    function descargaVideo($video_id){
+    function descargaVideo($video_id) {
         
     }
-    
+
     function url_exists($url) {
-        
-        echo $url. "<br>";
-        
-        
-        if(file_get_contents($url, FALSE, NULL, 0, 0) === false) return false;
+
+        echo $url . "<br>";
+
+
+        if (file_get_contents($url, FALSE, NULL, 0, 0) === false)
+            return false;
         return true;
     }
-    
-    
-    function obtenerVideo($id) {
-        
-        
-        $page = @file_get_contents('http://www.youtube.com/get_video_info?&video_id=' . $id);
 
-        preg_match('/token=(.*?)&thumbnail_url=/', $page, $token);
-
-        $token = urldecode($token[1]);
-
-//        $get = $title->video_details;
-
-        $url_array = array("http://youtube.com/get_video?video_id=" . $id . "&t=" . $token,
-            "http://youtube.com/get_video?video_id=" . $id . "&t=" . $token . "&fmt=18");
-
-        if (self::url_exists($url_array[1]) === true) {
-            $file = get_headers($url_array[1]);
-        } elseif (self::url_exists($url_array[0]) === true) {
-            $file = get_headers($url_array[0]);
-        }
-
-        print_r($file);
-        
+    function curlGet($URL) {
+        $ch = curl_init();
+        $timeout = 3;
+        curl_setopt($ch, CURLOPT_URL, $URL);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        /* if you want to force to ipv6, uncomment the following line */
+        //curl_setopt( $ch , CURLOPT_IPRESOLVE , CURLOPT_IPRESOLVE_V6);
+        $tmp = curl_exec($ch);
+        curl_close($ch);
+        return $tmp;
     }
+
+    function obtenerVideo($my_id) {
+
+
+        $my_video_info = 'http://www.youtube.com/get_video_info?&video_id=' . $my_id;
+        $my_video_info = self::curlGet($my_video_info);
+
+        echo $my_video_info;
+        
+        /* TODO: Check return from curl for status code */
+
+        var_dump(parse_str($my_video_info));
+    }
+
 }
 
 ?>
