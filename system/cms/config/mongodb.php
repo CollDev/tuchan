3a7e08dadf6bda2f2bb43c12ed63d4df8002879e
user <user@localhost>
2013-07-08 15:38:40 -0500

    Video Fixed

--- system/cms/config/mongodb.php
+++ system/cms/config/mongodb.php
@@ -20,10 +20,10 @@
 $config['mongo_host'] = '192.168.1.35';
 $config['mongo_port'] = 27017;
 $config['mongo_db'] = 'micanal_pre3';
-*/
-    
-// El Comercio  - DEV
+*/  
 
+// El Comercio  - DEV
+    
 $config['mongo_host'] = '10.203.31.139';
 $config['mongo_db'] = 'micanaldevmdb';
 $config['mongo_port'] = 27017;
