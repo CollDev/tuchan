6566fa3b432bb7ac4ee6f7b1a0799996b4093463
Joe Robles <joe.robles.pdj@gmail.com>
2013-07-11 20:04:36 -0500

    advanced features for table results

--- system/cms/views/cmsapi/layout.php
+++ system/cms/views/cmsapi/layout.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Title</title>
         <link href="/system/cms/themes/default/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="/system/cms/themes/default/js/tinytable/style.css" rel="stylesheet" />
         <link href="/system/cms/themes/default/css/styles.css" rel="stylesheet" />
         <link href="/system/cms/themes/default/img/favicon.ico" rel="shortcut icon" />
     </head>
@@ -16,6 +17,7 @@
         <script src="/system/cms/themes/default/js/jquery.js"></script>
         <script src="/system/cms/themes/default/js/jquery.mustache.js"></script>
         <script src="/system/cms/themes/default/bootstrap/js/bootstrap.min.js"></script>
+        <script src="/system/cms/themes/default/js/tinytable/packed.js"></script>
         <script src="/system/cms/themes/default/js/scripts.js"></script>
     </body>
 </html>
