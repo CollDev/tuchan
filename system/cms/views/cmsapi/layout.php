18162b95e0300ca561fda2ee04ad892cc6e232fe
Joe Robles <joe.robles.pdj@gmail.com>
2013-07-16 13:02:04 -0500

    lightbox for preview videos implemented

--- system/cms/views/cmsapi/layout.php
+++ system/cms/views/cmsapi/layout.php
@@ -6,6 +6,7 @@
         <title>Title</title>
         <link href="/system/cms/themes/default/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
         <link href="/system/cms/themes/default/js/tinytable/style.css" rel="stylesheet" />
+        <link href="/system/cms/themes/default/css/colorbox.css" rel="stylesheet" />
         <link href="/system/cms/themes/default/css/styles.css" rel="stylesheet" />
         <link href="/system/cms/themes/default/img/favicon.ico" rel="shortcut icon" />
     </head>
@@ -18,6 +19,7 @@
         <script src="/system/cms/themes/default/js/jquery.mustache.js"></script>
         <script src="/system/cms/themes/default/bootstrap/js/bootstrap.min.js"></script>
         <script src="/system/cms/themes/default/js/tinytable/packed.js"></script>
+        <script src="/system/cms/themes/default/js/jquery.colorbox-min.js"></script>
         <script src="/system/cms/themes/default/js/scripts.js"></script>
     </body>
 </html>
