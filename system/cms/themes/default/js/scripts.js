2efe69bf004c180e492941dff181cdf307318eb9
Joe Robles <joe.robles.pdj@gmail.com>
2013-07-26 12:59:15 -0500

    parentgetdata

--- system/cms/themes/default/js/scripts.js
+++ system/cms/themes/default/js/scripts.js
@@ -230,7 +230,7 @@ $(document).on('ready', function() {
     });
     $(document).on('click', 'a#use-this-video', function(e) {
         e.preventDefault();
-        console.log('add to hidden: video_id = "' + $(this).attr('href') + '"');
+        parent.getDataIframe(($(this).attr('href')));
     });
     $(document).on('click', 'a.corte_video', function() {
         $("div#cut_form_tab").html('<div class="text-center"><img src="/system/cms/themes/default/img/loading.gif" /></div>');
