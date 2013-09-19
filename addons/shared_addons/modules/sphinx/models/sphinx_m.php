37578cff92f9710decc5ce4e4050244e58178719
user <user@localhost>
2013-09-19 14:37:33 -0500

    sphinx

--- addons/shared_addons/modules/sphinx/models/sphinx_m.php
+++ addons/shared_addons/modules/sphinx/models/sphinx_m.php
@@ -101,7 +101,7 @@ class Sphinx_m extends CI_Model {
                 //$res = $result[0]["matches"];
                 
                 $res = array_merge($result[0]["matches"],$result[1]["matches"]);                                              
-                $res = array_unique_multi($res);
+//                $res = array_unique_multi($res);
               
                 
                 for ($i = 0; $i < count($res); $i++) {
