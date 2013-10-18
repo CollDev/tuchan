1d4a5cdb61f59eacc99ef2cbb2a8d0e4c4be0752
Joe Robles <joe.robles.pdj@gmail.com>
2013-10-18 17:26:18 -0500

    assets updated

--- system/cms/config/constants.php
+++ system/cms/config/constants.php
@@ -134,7 +134,7 @@ define('TIME_LIMIT',3500);
 |--------------------------------------------------------------------------
 */
 /* widget version */
-define('WIDGET_CACHE', 1380899059);
+define('WIDGET_CACHE', 1382135151);
 define('OOYALA_API_KEY', 'k4Z2wxOgjSCnJUu1lmb46ZNJLSPp.2EPYP');
 define('OOYALA_API_SECRET', 'vJUBsm-lgyn63ZrhuzUET1MgQwdL6pML2ot8CPng');
 
